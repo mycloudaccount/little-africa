534fce2517191cafc19d1bf24b493cf28af77ee4
Morgan Johnson <myibmaccount@yahoo.com>
2014-12-18 03:12:21 +0000

    from cloud 9

--- scripts/raindrop.js
+++ scripts/raindrop.js
@@ -1,33 +1,26 @@
-function Cloud(prefs){	
-	var sprite = window['cloud_' + prefs.type].clone();
-	sprite.name = 'cloud-'+guid();
+function RainDrop(prefs){	
+	var sprite = window['rainDrop'].clone();
+	sprite.name = 'rainDrop-'+guid();
 	sprite.x = prefs.x;
 	sprite.y = prefs.y;
 	sprite.scaleX = prefs.sx;
 	sprite.scaleY = prefs.sy;
-	sprite.alpha = prefs.alpha;
-	
+
 	var draw = function(){
 		
 		//will move by speed.
-		sprite.x += prefs.vx * WIND_SPEED;
-		sprite.y += prefs.vy * WIND_SPEED;
+		sprite.x += prefs.vx;
+		sprite.y += prefs.vy;
 		//will cause the object to wrap back
 		if ( sprite.x > (stage.canvas.width + sprite.image.width) || sprite.y > (stage.canvas.height + sprite.image.height) ) { 
-			sprite.x = -sprite.image.width; 
-			sprite.y = prefs.y; 
+			stage.removeChild(sprite);
 		}
-		
-		// randomly create and drop rain
-		var rainDrop = RainDrop({x:sprite.x,y:sprite.y});
-		
 		
 	};
 	
 	var handleClick = function(event)
 	{
 		console.log(sprite+' Hit!!');
-		//stage.removeChild(sprite);
 		stage.update();		
 	};	
 	sprite.addEventListener("click", handleClick);
